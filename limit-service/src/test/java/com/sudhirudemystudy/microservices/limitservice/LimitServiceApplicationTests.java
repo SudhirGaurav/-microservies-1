341de9c91fc4ceb7831d4ad36b65bbad92b1c353
SudhirGaurav <sudhirgauravec@gmail.com>
2023-04-03 18:13:41 +0530

    Refactoring code and fix Junit

--- limit-service/src/test/java/com/sudhirudemystudy/microservices/limitservice/LimitServiceApplicationTests.java
+++ limit-service/src/test/java/com/sudhirudemystudy/microservices/limitservice/LimitServiceApplicationTests.java
@@ -1,41 +1,19 @@
 package com.sudhirudemystudy.microservices.limitservice;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.MethodOrderer;
+import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.mockito.InjectMocks;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-
-
-//import com.example.fl.HBUtil;
-//import com.example.fl.WorkOrderTask;
-
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
 @SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-//@Slf4j
 class LimitServiceApplicationTests {
-	
-	@InjectMocks
-	  Object[] params;
 
-	//WorkOrderTask c = mock(WorkOrderTask.class);
+	@InjectMocks
+	Object[] params = new Object[5];
+	
 	@Test
 	void contextLoads() {
-		
-	System.out.println("Load........");
-	assertEquals("1", "2");
-	assertEquals("2", "1");
-	
-	
-		
-		
+		System.out.println("Load........");
+		assertEquals("Testing assertEquals", "1", "1");
 	}
-
 }
