0526149244d0dfbc442084af4c7720e554e59fb9
SudhirGaurav <sudhirgauravec@gmail.com>
2023-06-22 18:33:30 +0530

    connecting limit-service to spring-cloud-config to git repo

--- spring-cloud-config-server/src/main/java/com/sudhirudemystudy/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/sudhirudemystudy/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.sudhirudemystudy.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer // to enabling cloud config server 
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
