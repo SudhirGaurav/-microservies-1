1aef53fe0e87392f48d8b829a24f1e6e4c88aab9
SudhirGaurav <sudhirgauravec@gmail.com>
2023-06-27 09:54:54 +0530

    Feign loadbalancer talk to Eureka

--- currency-conversion-service/src/main/java/com/sudhirudemystudy/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/sudhirudemystudy/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 //Name is Application namer which is given in applicztion.properties
-@FeignClient(name = "currency-exchange" , url = "localhost:8000") // Hardcoded port is biggest issue, therefore Naming server came in concept  
+//@FeignClient(name = "currency-exchange" , url = "localhost:8000") // Hardcoded port is biggest issue, therefore Naming server came in concept  
+@FeignClient(name = "currency-exchange") // upper one s comment because of hardcodef port , now feign will talk to Eureka and talk to registered "currency-exchange" services
 public interface CurrencyExchangeProxy {
 
 	//This is the metod of corrency exchange service 
